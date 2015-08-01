0bdce31b4f58dcf14dd47129d9f7910202b43b2a
Sarah Henrie <Srhenrie@gmail.com>
2015-07-31 22:18:20 -0600

    Initial Commit

---
+++ RouteZ2/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  RouteZ2
+//
+//  Created by Sarah Henrie on 7/31/15.
+//  Copyright (c) 2015 Sarah Henrie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
