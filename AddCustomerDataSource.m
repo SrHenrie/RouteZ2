3c10385401765e82500b0501d4b171dfc6775bbc
Sarah Henrie <Srhenrie@gmail.com>
2015-08-08 23:09:24 -0600

    Added code to addCustomerView, set cells up.

--- AddCustomerDataSource.m
+++ AddCustomerDataSource.m
@@ -16,6 +16,9 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
     
+    if (section == 0) {
+        return 6;
+    }
     return 1;
 }
 
@@ -23,7 +26,24 @@
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"addCustomerCell"];
-    return cell;
+    UITableViewCell *cell1 = [tableView dequeueReusableCellWithIdentifier:@"addAppointmentCell"];
+    UITableViewCell *cell2 = [tableView dequeueReusableCellWithIdentifier:@"viewHistoryCell"];
+    UITableViewCell *cell3 = [tableView dequeueReusableCellWithIdentifier:@"nextAppointmentCell"];
     
+    if (indexPath.section == 0) {
+        return cell;
+    }if (indexPath.section == 1){
+        return cell1;
+    }if (indexPath.section == 2){
+        return cell2;
+    }else
+        return cell3;
 }
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 4;
+}
+
+
 @end
